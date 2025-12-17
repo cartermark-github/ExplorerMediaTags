7a829f5aded73dd969fbf5c5beeccfebf24c328f
Mark Carter <hawnyawk.net@gmail.com>
2025-12-17 07:25:36 -0600

    Added triple click/select all to the text box's.

---
+++ ExplorerTags/TripleClickSelectAllBehavior.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xaml.Behaviors; // Requires Microsoft.Xaml.Behaviors.Wpf NuGet
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ExplorerTags
+{
+    public class TripleClickSelectAllBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PreviewMouseDown += OnPreviewMouseDown;
+        }
+
+        private void OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 3)
+            {
+                AssociatedObject.SelectAll();
+                e.Handled = true; // Prevents the click from de-selecting text immediately
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PreviewMouseDown -= OnPreviewMouseDown;
+            base.OnDetaching();
+        }
+    }
+}
